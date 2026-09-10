9bba07a8e950600dd05c9d1e46cbdc6374a5565a
Himanshu <294himangshu@gmail.com>
2026-09-10 17:33:07 +0530

    fix: correct baseURL in axios configuration to use the correct port

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://187.127.163.17:302",
+  baseURL: import.meta.env.VITE_API_URL || "http://187.127.163.17:3022",
   // baseURL: "http://187.127.163.17:3022",
   timeout: 120000,
 });
